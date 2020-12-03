1f8293a0bdc099b34ac8d0f87bf841ba553036a5
I507747 <efecan.yilmaz@sap.com>
2020-12-03 15:23:02 +0100

    login improvements

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,13 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import SendIcon from '@material-ui/icons/Send';
 import useStyles from '../util/styles';
 import DirectionsIcon from '@material-ui/icons/Directions';
+import Util from '../util/util';
 
 function Login(props) {
     const classes = useStyles();
+    const [emailValidation, setEmailValidation] = useState(false);
+    const [emailValidationMessage, setEmailValidationMessage] = useState('');
+    const [email, setEmail] = useState('');
+    const [passwordValidation, setPasswordValidation] = useState(false);
+    const [passwordValidationMessage, setPasswordValidationMessage] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onEmailInput = (event) => {
+        var v = event.target.value;
+        checkEmailValid(v);
+    };
+
+    const checkEmailValid = (v) => {
+        if (!(v && v.length) || !Util().checkEmailRegex(v)) {
+            setEmailValidation(true);
+            setEmailValidationMessage('That is not an email address gov\'nor!!');
+            return false;
+        } else {
+            setEmailValidation(false);
+            setEmailValidationMessage('');
+            return true;
+        }
+    }
+    
+    const onPasswordInput = (event) => {
+        const v = event.target.value;
+        checkPasswordValid(v);
+    };
+
+    const checkPasswordValid = (v) => {
+        if (v && v.length > 0) {
+            setPasswordValidation(false);
+            setPasswordValidationMessage('');
+            return true;
+        } else {
+            setPasswordValidation(true);
+            setPasswordValidationMessage('Nooooooo! This is empty! You can\'t do this to me!');
+            return false;
+        }
+    }
+
+    const onLoginButtonClick = () => {
+        if (checkEmailValid(email) && checkPasswordValid(password)) {
+            alert('Yes!');
+        } else {
+            alert('No!');
+        }
+    }
 
     return (
         <div>
@@ -17,13 +66,13 @@ function Login(props) {
                         <h1 className={classes.title}>Show me what you got!!</h1>
                     </Grid>
                     <Grid className={classes.grid} item xs={12}>
-                        <TextField className={classes.textField} id="outlined-basic" label="What do they call you gov'nor?" variant="outlined" />
+                        <TextField className={classes.textField} id="emailTextField" label="What do they call you gov'nor?" value={email} onChange={(e)=>setEmail(e.target.value)} onBlur={onEmailInput} variant="outlined" error={emailValidation} helperText={emailValidationMessage} />
                     </Grid>
                     <Grid className={classes.grid} item xs={12}>
-                        <TextField className={classes.textField} id="outlined-basic" type="password" label="Fuck! I forgot the password!" variant="outlined" />
+                        <TextField className={classes.textField} id="passwordTextField" type="password" label="Fuck! I forgot the password!" value={password} onChange={(e)=>setPassword(e.target.value)} variant="outlined" onBlur={onPasswordInput} error={passwordValidation} helperText={passwordValidationMessage} />
                     </Grid>
                     <Grid className={classes.grid} item xs={12}>
-                        <Button variant="outlined" color="primary" startIcon={<SendIcon />}>Show time!</Button>
+                        <Button variant="outlined" color="primary" startIcon={<SendIcon />} onClick={onLoginButtonClick}>Show time!</Button>
                     </Grid>
                     <hr className={classes.lineBreak}/>
                     <Grid className={classes.grid} item xs={12}>
--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import SendIcon from '@material-ui/icons/Send';
 import DirectionsIcon from '@material-ui/icons/Directions';
 import useStyles from '../util/styles';
+import Util from '../util/util';
 
 function Login(props) {
     const classes = useStyles();
+    const [emailValidation, setEmailValidation] = useState(false);
+    const [emailValidationMessage, setEmailValidationMessage] = useState('');
+
+    const onEmailInput = (event) => {
+        if (!Util().checkEmailRegex(event.target.value)) {
+            setEmailValidation(true);
+            setEmailValidationMessage('That is not an email address gov\'nor!!');
+        } else {
+            setEmailValidation(false);
+            setEmailValidationMessage('');
+        }
+    };
 
     return (
         <div className={classes.box}>
@@ -16,16 +29,16 @@ function Login(props) {
                     <h1 className={classes.title}>Welcome to the Club Kido!</h1>
                 </Grid>
                 <Grid className={classes.grid} item xs={12}>
-                    <TextField className={classes.textField} id="outlined-basic" label="Give me your e-mail address! I need it!" variant="outlined" />
+                    <TextField className={classes.textField} id="emailTextField" label="Give me your e-mail address! I need it!" variant="outlined"  error={emailValidation} helperText={emailValidationMessage}/>
                 </Grid>
                 <Grid className={classes.grid} item xs={12}>
-                    <TextField className={classes.textField} id="outlined-basic" type="password" label="Choose! Your! Password! Wisely!" variant="outlined" />
+                    <TextField className={classes.textField} id="passwordTextField" type="password" label="Choose! Your! Password! Wisely!" variant="outlined" />
                 </Grid>
                 <Grid className={classes.grid} item xs={12}>
-                    <TextField className={classes.textField} id="outlined-basic" type="password" label="Could you please repeat that m'lady?" variant="outlined" />
+                    <TextField className={classes.textField} id="passwordRepeatTextField" type="password" label="Could you please repeat that m'lady?" variant="outlined" />
                 </Grid>
                 <Grid className={classes.grid} item xs={12}>
-                    <TextField className={classes.textField} id="outlined-basic" label="You shall not pass!" variant="outlined" />
+                    <TextField className={classes.textField} id="secretTextField" label="You shall not pass!" variant="outlined" />
                 </Grid>
                 <Grid className={classes.grid} item xs={12}>
                     <Button variant="outlined" color="primary" startIcon={<SendIcon />}>Let's roll!</Button>
