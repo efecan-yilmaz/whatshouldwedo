e7080cee40a5769dde6e98ab6b4e6601bfb9f0c0
I507747 <efecan.yilmaz@sap.com>
2020-12-09 16:43:53 +0100

    firebase

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './containers/Login';
 import SignUp from './containers/SignUp';
+import Firebase from './util/firebase';
 
 class App extends Component {
+    componentDidMount() {
+        const firebase = new Firebase();
+        firebase.initialize();
+    }
+
     render () {
         return (
             <BrowserRouter>
