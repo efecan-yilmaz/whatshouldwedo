e84346e1a3e0fb441048a53a0b7546817d4e330d
I507747 <efecan.yilmaz@sap.com>
2021-01-18 17:54:52 +0100

    addDialog

--- src/components/AddDialog.js
+++ src/components/AddDialog.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -8,9 +8,13 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useTheme } from '@material-ui/core/styles';
 
 export default function AddDialog(props) {
-    const [open, setOpen] = useState(true);
+    const [open, setOpen] = useState(false);
     const theme = useTheme();
     const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+    useEffect(() => {
+        setOpen(props.open);
+    }, [props.open])
 
     const handleClose = () => {
         setOpen(false);
